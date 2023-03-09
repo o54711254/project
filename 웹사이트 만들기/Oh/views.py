c080a85d573a2505c3e783119c9eaf87774f2795
o54711254 <o54711254@gmail.com>
2023-03-09 21:38:49 +0900

    질문 게시판 만들기

--- 웹사이트 만들기/Oh/views.py
+++ 웹사이트 만들기/Oh/views.py
@@ -1,8 +1,43 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from .models import Question
+from django.http import HttpResponseNotAllowed
+from .forms import QuestionForm, AnswerForm
 
 def index(request):
-    return HttpResponse("안녕하세요 제 웹사이트에 오신것을 환영합니다.")
+    question_list = Question.objects.order_by('-create_date')
+    context = {'question_list' : question_list}
+    return render(request, 'Oh/question_list.html', context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'Oh/question_detail.html', context)
+
+def answer_create(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    if request.method == "POST":
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.create_date = timezone.now()
+            answer.question = question
+            answer.save()
+            return redirect('Oh:detail', question_id=question.id)
+    else:
+        return HttpResponseNotAllowed('Only POST is possible.')
+    context = {'question': question, 'form': form}
+    return render(request, 'Oh/question_detail.html', context)
+
+def question_create(request):
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.create_date = timezone.now()
+            question.save()
+            return redirect('Oh:index')
+    else:
+        form = QuestionForm()
+    context = {'form': form}
+    return render(request, 'Oh/question_form.html', context)
